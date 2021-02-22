24092b7fe36f6671a52e066895ac29af5e76c174
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2021-02-22 18:00:52 +0900

    １つの画面にcardコンポーネントを表示する

---
+++ src/components/molecules/ValueCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="value-content">
+    <card-title :value="name"></card-title>
+    <card-number :value="value"></card-number>
+  </div>
+</template>
+<script>
+import CardTitle from "../atoms/CardTitle";
+import CardNumber from "../atoms/CardNumber";
+export default {
+  components: {
+    CardTitle,
+    CardNumber
+  },
+  props: {
+    name: String,
+    value: String
+  }
+};
+</script>
+<style scoped>
+.value-content {
+  display: inline-block;
+  margin: 40px 0 0 40px;
+  background-color: white;
+  border: 0 solid #aaa;
+  border-radius: 4px;
+  box-shadow: 2px 2px 2px 2px #ccc;
+  padding: 15px;
+}
+</style>
---
+++ src/components/pages/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="top">
+    <value-card name="ユーザー数 合計" value="250" style="height: 250px; width: 350px;"></value-card>
+    <value-card name="プレビュー数 合計" value="250" style="height: 250px; width: 450px;"></value-card>
+    <value-card name="プレビュー数 合計" value="350" style="height: 350px; width: 870px;"></value-card>
+    
+  </div>
+</template>
+<script>
+import ValueCard from "../molecules/ValueCard";
+export default {
+  components: {
+    ValueCard
+  }
+};
+</script>
+<style scoped>
+.top {
+  padding: 0px 0 0 200px;
+}
+</style>
